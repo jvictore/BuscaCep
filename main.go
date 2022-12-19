355ddb8419567268bf108d551844555bb79e8431
João Victor <72662270+jvictore@users.noreply.github.com>
2022-12-19 18:03:30 -0300

    Skeleton of the application

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	numParams := len(os.Args) - 1
+	if numParams == 0 {
+		println("0 parameters, ASK FOR INPUT")
+		return
+	}
+
+	println("More than 0 parameters, use the parameters")
+}
